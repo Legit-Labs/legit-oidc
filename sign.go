ff40e2b19ea6abd88752096a5113e706b064a4b4
gal-legit <gal@legitsecurity.com>
2022-10-18 17:44:01 +0300

    sign

--- sign.go
+++ sign.go
@@ -2,15 +2,47 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
 
 	"github.com/legit-labs/legit-attestation/pkg/legit_remote_attest"
 )
 
+func CmdExec(args ...string) (string, error) {
+
+	baseCmd := args[0]
+	cmdArgs := args[1:]
+
+	cmd := exec.Command(baseCmd, cmdArgs...)
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+
+	return string(out), nil
+}
+
+const OUT_PATH = "/tmp/result.intoto.jsnol"
+
 func sign(ctx context.Context, key string, payload legit_remote_attest.RemoteAttestationData) ([]byte, error) {
 	fmt.Printf("got: %#v\n", payload)
-	res := make(map[string]string)
-	res["test"] = "works"
-	return json.Marshal(res)
+
+	if err := payload.ApplyToEnv(); err != nil {
+		return nil, fmt.Errorf("failed to apply env: %v", err)
+	}
+
+	res, err := CmdExec("./generator", "attest", "--subjects", payload.SubjectsBase64, "--predicate", OUT_PATH)
+	if err != nil {
+		return nil, fmt.Errorf("failed to attest: %v", err)
+	}
+	log.Printf("attest output: %v\n", res)
+
+	output, err := os.ReadFile(OUT_PATH)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read output: %v", err)
+	}
+
+	return output, nil
 }
