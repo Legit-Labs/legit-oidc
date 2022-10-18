733a53538a36618fb0ae4c633893e8523d34564a
gal-legit <gal@legitsecurity.com>
2022-10-18 19:23:24 +0300

    output

--- sign.go
+++ sign.go
@@ -9,16 +9,16 @@ import (
 	"github.com/legit-labs/legit-attestation/pkg/legit_remote_attest"
 )
 
-func CmdExec(args ...string) error {
+func CmdExec(args ...string) ([]byte, error) {
 
 	baseCmd := args[0]
 	cmdArgs := args[1:]
 
 	cmd := exec.Command(baseCmd, cmdArgs...)
-	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	return cmd.Run()
+	output, err := cmd.Output()
+	return output, err
 }
 
 func sign(ctx context.Context, keyRef string, payload legit_remote_attest.RemoteAttestationData) ([]byte, error) {
@@ -30,14 +30,9 @@ func sign(ctx context.Context, keyRef string, payload legit_remote_attest.Remote
 
 	os.Setenv("PRIVATE_KEY_PATH", keyRef)
 
-	err := CmdExec("./generator", "attest", "--subjects", payload.SubjectsBase64, "--signature", "", "--predicate", "")
+	output, err := CmdExec("./generator", "attest", "--subjects", payload.SubjectsBase64, "--signature", "", "--predicate", "")
 	if err != nil {
 		return nil, fmt.Errorf("failed to attest: %v", err)
-	}
-
-	output, err := os.ReadFile(OUT_PATH)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read output: %v", err)
 	}
 
 	return output, nil
