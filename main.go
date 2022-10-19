c9dea7da60c354d0110810b50f09c1c223750799
gal-legit <gal@legitsecurity.com>
2022-10-19 17:35:07 +0300

    verify

--- main.go
+++ main.go
@@ -24,7 +24,11 @@ func jwtPost(w http.ResponseWriter, req *http.Request) {
 	jwtB64 := req.Header.Get("jwt")
 	verifier := legit_remote_provenance_server.NewJwtVerifier(verifyJwt)
 	if err := verifier.Verify(jwtB64); err != nil {
-		log.Panicf("failed to verify jwt token [verify=%v]: %v", verifyJwt, err)
+		if verifyJwt {
+			log.Panicf("failed to verify jwt token: %v", err)
+		} else {
+			log.Panicf("continue although JWT verification failed because: %v", err)
+		}
 	}
 
 	var payload legit_remote_provenance.RemoteAttestationData
